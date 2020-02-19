6799fbd69844f18d29ea8661f6b969a6a095528a
munene19 <munenexv@gmail.com>
2020-02-19 15:53:02 +0300

    models created

--- award/models.py
+++ award/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=30)
+    project_description = models.TextField(max_length=100)
+    project_url = 
+    posted_on = models.DateTimeField(auto_now_add=True)
+    
