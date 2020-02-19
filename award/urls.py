6ed32e7b113761fc550c23eaf0ac2b94f2327052
munene19 <munenexv@gmail.com>
2020-02-19 12:26:42 +0300

    First app created

---
+++ award/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+url patterns=[
+
+
+]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
