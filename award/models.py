8c0161eb0129a57869ca2d1e3f57c543c1200395
munene19 <munenexv@gmail.com>
2020-02-20 00:31:23 +0300

    update templates

--- award/models.py
+++ award/models.py
@@ -1,21 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
+from django.utils import timezone
+from django.urls import reverse
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    profile_picture = models.ImageField(upload_to = 'profilepics/')
-    bio = models.TextField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+    bio = models.TextField(max_length=100, blank=True)
+    contact = models.TextField(max_length=100, blank=True)
+    # votes = models.IntegerField(default = 0)
+
 
     def __str__(self):
-        return self.first_name
+        return f'{self.user.username} Profile'
+    
+    def save(self, **kwargs):
+        super().save()
 
-    def save_profile(self):
-        self.save()
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user = id).first()
+        return details
 
 class Project(models.Model):
-    project_name = models.CharField(max_length=30)
-    project_description = models.TextField(max_length=100)
-    project_url = 
-    posted_on = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/')
+    title = models.CharField(max_length =100)
+    url = models.CharField(max_length =80)
+    detail_desciption=models.TextField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('project-detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-pk']
+ 
+    def save_project(self):
+        self.save()
+
+    @classmethod
+    def search_by_projectname(cls,idea):
+        projects = cls.objects.filter(title__icontains=idea)
+        return projects
--- award/urls.py
+++ award/urls.py
@@ -1,9 +1,21 @@
 from django.conf.urls import url
 from django.contrib import admin
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from .views import ProjectDetailView, ProjectListView, ProjectCreateView, ProjectUpdateView
+from . import views 
 
 urlpatterns=[
     url(r'^admin/', admin.site.urls),
+    url('', views.home, name='home'),
+    url(r'^project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    url(r'^project/new/', ProjectCreateView.as_view(), name='project-create'),
+    url(r'^project/<int:pk>/update/', ProjectUpdateView.as_view(), name='project-update'),
+    url(r'^api/profiles/', views.ProfileList.as_view()),
+    url(r'^api/projects/', views.ProjectList.as_view()),
+    url(r'^vote/<project/<int:pk/',views.vote, name='vote'),
+    url(r'^search/',views.search_project, name='search_project'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
